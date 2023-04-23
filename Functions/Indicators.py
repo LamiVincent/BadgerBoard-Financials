7790d669ea0a5e881a8f1864bad71ecb0a6d58d4
LamiVincent <70013529+LamiVincent@users.noreply.github.com>
2023-04-23 13:54:23 +0200

    MACD added

--- Functions/Indicators.py
+++ Functions/Indicators.py
@@ -31,3 +31,26 @@ def get_my_stock_data(account, period_p, addAll = True):
             d_h1y[j]['Cumulative Return'] = (1 + d_h1y[j]['returns']).cumprod()
             
     return d_ib, d_h1y
+
+
+def get_ema(data, period=0, column='Close'):
+    data['ema' + str(period)] = data[column].ewm(ignore_na=False, min_periods=period, com=period, adjust=True).mean()
+    
+    return data
+
+def get_macd(data, period_long=26, period_short=12, period_signal=9, column='Close'):
+    remove_cols = []
+    if not 'ema' + str(period_long) in data.columns:
+        data = get_ema(data, period_long, column=column)
+        remove_cols.append('ema' + str(period_long))
+
+    if not 'ema' + str(period_short) in data.columns:
+        data = get_ema(data, period_short, column=column)
+        remove_cols.append('ema' + str(period_short))
+
+    data['macd_val'] = data['ema' + str(period_short)] - data['ema' + str(period_long)]
+    data['macd_signal_line'] = data['macd_val'].ewm(ignore_na=False, min_periods=0, com=period_signal, adjust=True).mean()
+
+    data = data.drop(remove_cols, axis=1)
+        
+    return data
